bef37c7ba6113bbe47b4d1084c75b720ee3b4123
taehwan-jang <74484564+taehwan-jang@users.noreply.github.com>
2021-03-14 21:45:17 +0900

    2021.03.14

--- K01Java/src/ex06array/QuTwoDimLotate.java
+++ K01Java/src/ex06array/QuTwoDimLotate.java
@@ -16,13 +16,21 @@ public class QuTwoDimLotate {
 	
 	public static void main(String[] args) {
 		
-		int[][] arr = new int[3][3];
+//		int[][] arr = new int[3][3];
+//		
+//		for(int i=0 ; i<arr.length ; i++) {
+//			for(int j=0 ; j<arr[i].length ; j++) {
+//				if(i==0) {
+//					arr[i][j] += i+j+1;
+//				}
+//			}
+//		}
 		
-		for(int i=0 ; i<arr.length ; i++) {
-			for(int j=0 ; j<arr[i].length ; j++) {
-				arr[i][j] += i+j+1;
-			}
-		}
+		int[][] arr = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
 		System.out.println("[swap메소드 호출전]");
 		showarr(arr);
 		arrSwap(arr);
